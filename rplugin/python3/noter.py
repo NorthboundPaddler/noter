82f048a19dca397e42c8041f4f3a1a15e7814efc
Aaron Thompson <aaroncthompson1@example.com>
2023-08-05 11:57:02 -0500

    debug print checkFile result

--- rplugin/python3/noter.py
+++ rplugin/python3/noter.py
@@ -52,7 +52,7 @@ class NoterPlugin(object):
         # add a record to the directory's Noter DB
         # TODO Check if file already exists before adding it
         exists = self.checkFile()
-        if exists == True:
+        if exists is True:
 
             # Add record to DB using file name and path
             fileName, filePath = self.getFileMetadata()
@@ -60,7 +60,7 @@ class NoterPlugin(object):
             result = con.execute(
                 f'INSERT INTO notes (name, path) VALUES ("{fileName}", "{filePath}")')
             con.commit()
-            self.nvim.out_write("Current file added to noter.db\n")
+            self.nvim.out_write(f"checkFile returned {exists}, and Current file added to noter.db\n")
             return result
         else:
             raise Exception("This file already exists in noter.db")
