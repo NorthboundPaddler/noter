c234b3734227b18aa477201180683f9542fda9f2
Aaron Thompson <aaroncthompson1@example.com>
2023-08-04 20:41:12 -0500

    Upload of initial files

---
+++ rplugin/python3/noter.py
@@ -0,0 +1,31 @@
+import pynvim
+import sqlite3
+import os
+
+
+# Hardcoded env variables - for now...
+dbPath = r"./noter.db"
+
+
+@pynvim.plugin
+class NoterPlugin(object):
+    def __init__(self, nvim):
+        self.nvim = nvim
+
+    @pynvim.function("TestNoter")
+    def testNoter(self):
+        # Quick test to output to nvim console
+        return "Noter Plugin operational"
+
+    @pynvim.function("NoterBuildDB")
+    def buildDB(self):
+        # Check for an existing SQLite DB
+        # Create one if it is missing
+        # otherwise flash the user with an err
+        if os.path.exists(dbPath):
+            raise Exception("The DB has already been created")
+        con = sqlite3.connect(dbPath)
+        cur = con.cursor()
+        cur.execute("CREATE TABLE notes(id, name, path)")
+
+        return
